cb2ea79e9d2ad8834eaeaf1a6d6f0896d2421bbf
danielsaggir <danielsaggir@gmail.com>
2026-06-19 17:15:58 +0300

    Add deleteNote function and update routes to support note deletion

--- server/src/controllers/notes.controller.js
+++ server/src/controllers/notes.controller.js
@@ -1,9 +1,11 @@
 const notes =require("../data/notes.data");
 
+//get all notes
 const getNotes = (req, res) => {
     res.json(notes);
 };
 
+//create note
 const createNote = (req, res) => {
     const {title, content} = req.body;
 
@@ -21,4 +23,17 @@ const createNote = (req, res) => {
     res.status(201).json(newNote);
 };
 
-module.exports = { getNotes, createNote };
+//delete note by id
+const deleteNote = (req, res) => {
+    ///const {id} = req.params;
+    const id = Number(req.params.id);
+    const noteIndex = notes.findIndex((note) => note.id === id);
+    if (noteIndex === -1){
+        return res.status(404).json({error: "Note not found"});
+    }
+
+    notes.splice(noteIndex, 1);
+    res.json({message: "Note deleted successfully"});
+};
+
+module.exports = { getNotes, createNote, deleteNote };
--- server/src/routes/notes.routes.js
+++ server/src/routes/notes.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getNotes, createNote } = require("../controllers/notes.controller");
+const { getNotes, createNote, deleteNote } = require("../controllers/notes.controller");
 
 const router = express.Router();
 
 router.get("/", getNotes);
 router.post("/", createNote);
+router.delete("/:id", deleteNote);
 
 module.exports = router;
